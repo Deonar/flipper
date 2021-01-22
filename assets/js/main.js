bf4f382f400ec1f6c562d0ee0ccb03f54624b70b
Yevhenii <zhenyaliho94@gmail.com.com>
2021-01-22 17:47:45 +0200

    fix1

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,37 +11,6 @@ jQuery(document).ready(function ($) {
     $('#mobMenu').removeClass('active');
   });
 
-  //===========================  main-slider
-  $('#main-slider').slick({
-    arrows: false,
-    dots: true,
-    slidesToShow: 1,
-    autoplay: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear',
-    adaptiveHeight: true,
-  });
-  $('#team-slider').slick({
-    dots: false,
-    slidesToShow: 3,
-    adaptiveHeight: true,
-    responsive: [
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  });
-
   //scroll-header-menu
   var scrollPos = 0;
   $(document).scroll(function () {
@@ -51,51 +20,16 @@ jQuery(document).ready(function ($) {
       $('#header').removeClass('h-fixed');
     }
 
-    // var st = $(this).scrollTop();
-    // if (st < scrollPos) {
-    //   $('#header').removeClass('active');
-    // } else {
-    //   $('#header').addClass('active');
-    // }
-    // scrollPos = st;
-    // if (scrollPos <= 200) {
-    //   $('#header').removeClass('active');
-    // }
-  });
-
-  //======================== POPUP
-  $('.popup').magnificPopup({
-    mainClass: 'mfp-fade',
-  });
-
-  //pricing hover
-  $('.pricing-item').on('click', function (e) {
-    $(this).toggleClass('active');
-  });
-  if ($(window).width() <= '767') {
-    $('.pricing-item').removeClass('_hover');
-  }
-
-  //pricing calculate
-
-  function calcPriceRow(sumOut, pricingItem, dataPrice) {
-    $(pricingItem).on('click', function () {
-      let sum = 0;
-      $(pricingItem).each(function (index, element) {
-        if ($(element).hasClass('active')) {
-          sum += +$(this).attr(dataPrice);
-        }
-      });
-      $(sumOut).text(sum);
-    });
-  }
-  calcPriceRow('#sum-price-patent', '.pricing-item-patent', 'data-price-patent');
-  calcPriceRow('#sum-price-fto', '.pricing-item-fto', 'data-price-fto');
-
-  $('#list-price').on('click', function (e) {
-    let resultPrice = 0;
-    resultPrice = +$('.custom-radio__input[type="radio"]:checked').closest('.table-row').find('.pricing-result__num').text();
-    $('#result-price').text(resultPrice);
+    var st = $(this).scrollTop();
+    if (st < scrollPos) {
+      $('#header').removeClass('active');
+    } else {
+      $('#header').addClass('active');
+    }
+    scrollPos = st;
+    if (scrollPos <= 200) {
+      $('#header').removeClass('active');
+    }
   });
 
   //end
